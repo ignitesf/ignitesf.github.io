abc21591cdc5ca372865c26541441dbe4dc37849
Mick Thompson <dthompson@gmail.com>
2014-01-09 10:32:19 -0800

    initial commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(function(){
+
+    var headings = $(".promo-title h2").toArray();
+    var count = 0;
+
+    function switchHeading(){
+        $(".promo-title h2").removeClass("show");
+        $(headings[count]).addClass("show");
+        count += 1;
+        if(count > 2)
+            count =0;
+
+        setTimeout(switchHeading, 5000);
+    }
+    switchHeading();
+});
